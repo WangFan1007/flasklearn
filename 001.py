e0afe6a155edd3848dcca836c185a473f566f29c
home ubuntu <1036815077@qq.com>
2019-12-26 19:13:29 +0800

    IGNORE

--- 001.py
+++ 001.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
-# app = Flask(__name__)
 app = Flask(__name__)
+
+
 @app.route('/hello')
 def hello():
     return "Hello World"
 
 
-app.run('0.0.0.0',5000,debug=True)
+app.run('0.0.0.0', 5000, debug=True)
